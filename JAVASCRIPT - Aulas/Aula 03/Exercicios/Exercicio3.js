7e40e23b09830e1dab4ddcd7bd275a04cfe6ae0a
Sarah <sntiago.sm@gmail.com>
2024-10-09 11:59:58 -0300

    JS 03

---
+++ JAVASCRIPT - Aulas/Aula 03/Exercicios/Exercicio3.js
@@ -0,0 +1,18 @@
+//APLICANDO METODOS
+
+
+
+//Crie uma string para apresentar o resultado.
+
+
+
+//Crie uma variável com o seu nome;
+let nomeCompleto = "Sarah Santiago";
+let partesNome = nomeCompleto.split();
+
+
+
+//Depois utilizando o método Replace troque o seu nome do meio pelo último sobrenome;
+let trocaNome = nomeCompleto.replace('Sarah', 'Santiago');
+let
+
